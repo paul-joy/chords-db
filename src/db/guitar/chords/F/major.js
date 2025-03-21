c994038b5b1f3781215e7206cf5461fec6430797
Pavel Korchyk <web.development.pavel@gmail.com>
2025-03-21 16:37:14 +0300

    fix tests for F major

--- src/db/guitar/chords/F/major.js
+++ src/db/guitar/chords/F/major.js
@@ -1,31 +1,31 @@
 export default {
-  key: 'F',
-  suffix: 'major',
+  key: "F",
+  suffix: "major",
   positions: [
     {
-      frets: '133211',
-      fingers: '134211',
+      frets: "133211",
+      fingers: "134211",
       barres: 1,
       capo: true,
     },
     {
-      frets: 'xx3211',
-      fingers: 'xx3211',
+      frets: "xx3211",
+      fingers: "003211",
       barres: 1,
     },
     {
-      frets: 'xx3565',
-      fingers: '001243',
+      frets: "xx3565",
+      fingers: "001243",
     },
     {
-      frets: 'x87565',
-      fingers: '043121',
+      frets: "x87565",
+      fingers: "043121",
       barres: 5,
       capo: true,
     },
     {
-      frets: 'x8aaa8',
-      fingers: '012341',
+      frets: "x8aaa8",
+      fingers: "012341",
       barres: 8,
       capo: true,
     },
